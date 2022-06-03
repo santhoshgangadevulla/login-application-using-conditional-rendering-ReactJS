a674c973567f2f52b7ab0bf6f864f495e583e85a
root <santhoshkumar92272@gmail.com>
2022-06-03 07:15:29 +0000

    login application using condtional rendering method in ReactJS

---
+++ src/components/Home/index.js
@@ -0,0 +1,32 @@
+// Write your code here
+import {Component} from 'react'
+import Login from '../Login'
+import Message from '../Message'
+import Logout from '../Logout'
+import './index.css'
+
+class Home extends Component {
+  state = {isLoggedIn: false}
+
+  onClickButton = () => {
+    this.setState(prevState => ({isLoggedIn: !prevState.isLoggedIn}))
+  }
+
+  render() {
+    const {isLoggedIn} = this.state
+    return (
+      <div className="container">
+        <div className="sub-container">
+          <Message isLoggedIn={isLoggedIn} />
+          {isLoggedIn ? (
+            <Logout logout={this.onClickButton} />
+          ) : (
+            <Login login={this.onClickButton} />
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
